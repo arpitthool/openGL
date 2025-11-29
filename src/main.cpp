b40ecfdd21220dd63c5e3cdf717e32becafdcb86
Arpit Thool <arpitthool@vt.edu>
2025-11-28 22:34:01 -0500

    - draw 3 traingles 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -30,12 +30,23 @@ int main() {
     // tell the GLFW that we are using the core profile i.e. only the modern functions
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-
+    // to save memory, we just need to store the vertices of the outer triangle and the inner midpoints to draw all 3 triangles
     GLfloat vertices[] = {
         -0.5f, -0.5f * float(sqrt(3))/3, 0.0f, // left bottom corner of the triangle
         0.5f, -0.5f* float(sqrt(3))/3, 0.0f, // right bottom corner of the triangle
         0.0f, 0.5f* float(sqrt(3))*2/3, 0.0f, // top corner of the triangle
+        -0.5f/2, 0.5f * float(sqrt(3))/6, 0.0f, // inner left 
+        0.5f/2, 0.5f* float(sqrt(3))/6, 0.0f, // inner right 
+        0.0f, -0.5f* float(sqrt(3))/3, 0.0f, // inner bottom
+
     }; // the position must be between -1.0 and 1.0, because the viewport is also between -1.0 and 1.0
+
+    // to save memory, we can use indices to draw the triangles
+    GLint indices[] = { 
+        0, 3, 5, // lower left triangle
+        3, 2, 4, // lower right triangle
+        5, 4, 1, // upper triangle
+    };
 
     int screenWidth = 800;
     int screenHeight = 600;
@@ -70,15 +81,19 @@ int main() {
     glDeleteShader(vertexShader); // Delete the vertex shader
     glDeleteShader(fragmentShader); // Delete the fragment shader
 
-    GLuint VAO, VBO;
+    GLuint VAO, VBO, EBO;
     int noOfObjects = 1, noOfVertices = 3;
     glGenVertexArrays(noOfObjects, &VAO); // Generate a vertex array object
     glGenBuffers(noOfObjects, &VBO); // Generate a buffer object
+    glGenBuffers(noOfObjects, &EBO); // Generate a buffer object for the indices
 
     glBindVertexArray(VAO); // Bind the vertex array object
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO); // Bind the buffer object
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Copy the vertices data to the buffer
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); // Bind the buffer object for the indices
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); // Copy the indices data to the buffer
 
     int positionOfVertexAttribute = 0;
     glVertexAttribPointer(positionOfVertexAttribute, noOfVertices, GL_FLOAT, GL_FALSE, noOfVertices * sizeof(float), (void*)0); // Set the vertex attribute pointer
@@ -86,6 +101,7 @@ int main() {
 
     glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbind the buffer object
     glBindVertexArray(0); // Unbind the vertex array object
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); // Unbind the buffer object for the indices
 
     glClearColor(0.07f, 0.13f, 0.17f, 1.0f); // Set the background color to a light blue color, 
     // the last parameter is the alpha value which is 1.0f for fully opaque
@@ -103,7 +119,9 @@ int main() {
         glUseProgram(shaderProgram); // Use the shader program
         glBindVertexArray(VAO); // Bind the vertex array object
         int startIndex = 0;
-        glDrawArrays(GL_TRIANGLES, startIndex, noOfVertices); // Draw the triangles
+        // glDrawArrays(GL_TRIANGLES, startIndex, noOfVertices); // Draw the triangles
+        int noOfIndices = sizeof(indices) / sizeof(indices[0]);
+        glDrawElements(GL_TRIANGLES, noOfIndices, GL_UNSIGNED_INT, 0); // Draw the triangles using the indices
         glfwSwapBuffers(window); // Swap the back buffer with the front buffer
 
         glfwPollEvents(); // Poll for events because GLFW needs to process the events
@@ -112,6 +130,7 @@ int main() {
     glDeleteVertexArrays(noOfObjects, &VAO); // Delete the vertex array object
     glDeleteBuffers(noOfObjects, &VBO); // Delete the buffer object
     glDeleteProgram(shaderProgram); // Delete the shader program
+    glDeleteBuffers(noOfObjects, &EBO); // Delete the buffer object for the indices
 
     glfwDestroyWindow(window); // Destroy the window
     glfwTerminate(); // Clean up GLFW when the program is done
