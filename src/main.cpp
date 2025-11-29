21084606c81d1c3869bd2a7aaa33b15561796cdf
Arpit Thool <arpitthool@vt.edu>
2025-11-28 20:32:46 -0500

    - creating and displaying a glfw window until closed 	modified:   CMakeLists.txt 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,29 @@ int main() {
     
     glfwInit(); // Initialize GLFW
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    int screenWidth = 800;
+    int screenHeight = 600;
+    GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "OpenGL Project", NULL, NULL); // Create a window
+
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window); // Make the window's context current
+
+    while (!glfwWindowShouldClose(window)) { // loop until the window is closed
+        glfwPollEvents(); // Poll for events because GLFW needs to process the events
+    }
+
+
+    glfwDestroyWindow(window); // Destroy the window
     glfwTerminate(); // Clean up GLFW when the program is done
 
-    return 0;
+    return 0; // Return success
 }
