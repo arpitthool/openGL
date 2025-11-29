e81d4cd9bc9aa1464053051732be7c0ddb883066
Arpit Thool <arpitthool@vt.edu>
2025-11-29 18:15:27 -0500

    - drawing a square instead of triangles 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,21 +9,36 @@
 #include "../dependencies/stb/stb_image.h"
 
 // to save memory, we just need to store the vertices of the outer triangle and the inner midpoints to draw all 3 triangles
+// GLfloat vertices[] =
+// { //               COORDINATES                  /     COLORS           //
+// 	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
+// 	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
+// 	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
+// 	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
+// 	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
+// 	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
+// }; // the position must be between -1.0 and 1.0, because the viewport is also between -1.0 and 1.0
+
+// // to save memory, we can use indices to draw the triangles
+// GLuint indices[] = { 
+//     0, 3, 5, // lower left triangle
+//     3, 2, 4, // lower right triangle
+//     5, 4, 1, // upper triangle
+// };
+
+// Square
 GLfloat vertices[] =
 { //               COORDINATES                  /     COLORS           //
-	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
-	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
-	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
-	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
-	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
-	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
+	-0.5f, -0.5f , 0.0f,     1.0f, 0.0f,  0.0f, // Lower left corner
+	 -0.5f, 0.5f , 0.0f,     0.0f, 1.0f,  0.0f, // upper left corner
+	 0.5f,  0.5f , 0.0f,     0.0f, 0.0f,  1.0f, // Upper right corner
+	 0.5f, -0.5f , 0.0f,     1.0f, 1.0f,  1.0f  // Lower right corner
 }; // the position must be between -1.0 and 1.0, because the viewport is also between -1.0 and 1.0
 
 // to save memory, we can use indices to draw the triangles
 GLuint indices[] = { 
-    0, 3, 5, // lower left triangle
-    3, 2, 4, // lower right triangle
-    5, 4, 1, // upper triangle
+    0, 2, 1, // upper triangle
+    0, 3, 2, // lower triangle
 };
 
 int main() {
