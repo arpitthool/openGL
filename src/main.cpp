0ea147ad87050d1eab515bb832cb1595c46fc17b
Arpit Thool <arpitthool@vt.edu>
2025-11-29 17:46:58 -0500

    - added RGB color values to the vertex points and OpenGL automatically shaded from one verter to another 	modified:   headers/VAO.h 	modified:   resources/shaders/default.frag 	modified:   resources/shaders/default.vert 	modified:   src/VAO.cpp 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,14 @@
 #include "../headers/EBO.h"
 
 // to save memory, we just need to store the vertices of the outer triangle and the inner midpoints to draw all 3 triangles
-GLfloat vertices[] = {
-    -0.5f, -0.5f * float(sqrt(3))/3, 0.0f, // left bottom corner of the triangle
-    0.5f, -0.5f* float(sqrt(3))/3, 0.0f, // right bottom corner of the triangle
-    0.0f, 0.5f* float(sqrt(3))*2/3, 0.0f, // top corner of the triangle
-    -0.5f/2, 0.5f * float(sqrt(3))/6, 0.0f, // inner left 
-    0.5f/2, 0.5f* float(sqrt(3))/6, 0.0f, // inner right 
-    0.0f, -0.5f* float(sqrt(3))/3, 0.0f, // inner bottom
-
+GLfloat vertices[] =
+{ //               COORDINATES                  /     COLORS           //
+	-0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
+	 0.5f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
+	 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
+	-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
+	 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
+	 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f  // Inner down
 }; // the position must be between -1.0 and 1.0, because the viewport is also between -1.0 and 1.0
 
 // to save memory, we can use indices to draw the triangles
@@ -69,7 +69,9 @@ int main() {
 	EBO EBO1(indices, sizeof(indices));
 
 	// Links VBO to VAO
-	VAO1.LinkVBO(VBO1, 0);
+	// VAO1.LinkVBO(VBO1, 0);
+    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
 	VBO1.Unbind();
