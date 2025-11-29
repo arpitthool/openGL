829d9a5013ebc42ac9fb6ea8b51682bc3ab0d3f2
Arpit Thool <arpitthool@vt.edu>
2025-11-28 20:43:02 -0500

    - showing color on the openGL window 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,11 @@ int main() {
     
     glfwInit(); // Initialize GLFW
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // tell the GLFW version we are using i.e. 4.6
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+
+    // tell the GLFW that we are using the core profile i.e. only the modern functions
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     int screenWidth = 800;
@@ -21,6 +24,19 @@ int main() {
     }
 
     glfwMakeContextCurrent(window); // Make the window's context current
+
+    gladLoadGL(); // Load OpenGL using GLAD
+
+    glViewport(0, 0, screenWidth, screenHeight); // Set the viewport to the entire window, the viewport is the area of the window that the graphics are rendered to
+    // the first two parameters are the x and y coordinates of the viewport, the last two parameters are the width and height of the viewport
+
+    glClearColor(0.07f, 0.13f, 0.17f, 1.0f); // Set the background color to a light blue color, 
+    // the last parameter is the alpha value which is 1.0f for fully opaque
+
+    glClear(GL_COLOR_BUFFER_BIT); // clean the buffer and assign new to it
+
+    // now to show the color we need to swap the back buffer with the front buffer
+    glfwSwapBuffers(window); // Swap the back buffer with the front buffer
 
     while (!glfwWindowShouldClose(window)) { // loop until the window is closed
         glfwPollEvents(); // Poll for events because GLFW needs to process the events
